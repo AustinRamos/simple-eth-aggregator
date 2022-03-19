1126f3d57ea8add62e07f536a21d3b99328b3b15
Austin Ramos <austinramos@Austins-MacBook-Pro.local>
2022-03-18 17:16:24 -0700

    making PriceAggregator child component of App, to introuce lending/liquidation data.

---
+++ simple-eth-aggregator/src/components/PriceAggregator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// displays a page header
+
+export default function Header() {
+  return (
+      <div>
+
+      </div>
+  );
+}
--- simple-eth-aggregator/src/components/index.js
+++ simple-eth-aggregator/src/components/index.js
@@ -2,3 +2,4 @@ export { default as PriceRow } from "./PriceRow";
 export { default as Header } from "./Header";
 export { default as PriceInfo } from "./PriceInfo";
 export { default as Footer } from "./Footer";
+export { default as PriceAggregator } from "./PriceAggregator";
